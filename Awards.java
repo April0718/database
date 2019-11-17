ff00dcb0aa130854bd745e39590505afe39472ae
April0718 <57655214+April0718@users.noreply.github.com>
2019-11-17 19:34:54 +0800

    Create Awards.java

---
+++ Awards.java
@@ -0,0 +1,27 @@
+package DouBan;
+
+public class Awards {
+	private int Fid;
+	private String Awname;
+	private String Awtime;
+	public int getFid() {
+		return Fid;
+	}
+	public void setFid(int fid) {
+		Fid = fid;
+	}
+	public String getAwname() {
+		return Awname;
+	}
+	public void setAwname(String awname) {
+		Awname = awname;
+	}
+	public String getAwtime() {
+		return Awtime;
+	}
+	public void setAwtime(String awtime) {
+		Awtime = awtime;
+	}
+	
+	
+}
