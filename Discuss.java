6dbafdf8af00a15a7d9d7adc2affc6fa931b5b8d
April0718 <57655214+April0718@users.noreply.github.com>
2019-11-17 19:37:59 +0800

    Create Discuss.java

---
+++ Discuss.java
@@ -0,0 +1,62 @@
+package DouBan;
+
+public class Discuss {
+	private int Did;
+	private int Uid;
+	private int Fid;
+	private String Dtitle;
+	private String D_content;
+	private int count;
+	private int r_cnt;
+	private String D_time;
+	public int getDid() {
+		return Did;
+	}
+	public void setDid(int did) {
+		Did = did;
+	}
+	public int getUid() {
+		return Uid;
+	}
+	public void setUid(int uid) {
+		Uid = uid;
+	}
+	public int getFid() {
+		return Fid;
+	}
+	public void setFid(int fid) {
+		Fid = fid;
+	}
+	public String getDtitle() {
+		return Dtitle;
+	}
+	public void setDtitle(String dtitle) {
+		Dtitle = dtitle;
+	}
+	public String getD_content() {
+		return D_content;
+	}
+	public void setD_content(String d_content) {
+		D_content = d_content;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getR_cnt() {
+		return r_cnt;
+	}
+	public void setR_cnt(int r_cnt) {
+		this.r_cnt = r_cnt;
+	}
+	public String getD_time() {
+		return D_time;
+	}
+	public void setD_time(String d_time) {
+		D_time = d_time;
+	}
+	
+	
+}
