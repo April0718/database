9e5f504692e491ae64290e4ca242d4a7aaf0389c
April0718 <57655214+April0718@users.noreply.github.com>
2019-11-17 19:38:22 +0800

    Create Tag.java

---
+++ Tag.java
@@ -0,0 +1,25 @@
+package DouBan;
+public class Tag{
+	private int fid;
+	private int tnum;
+	private String tcontent;
+	public int getFid() {
+		return fid;
+	}
+	public void setFid(int fid) {
+		this.fid = fid;
+	}
+	public int getTnum() {
+		return tnum;
+	}
+	public void setTnum(int tnum) {
+		this.tnum = tnum;
+	}
+	public String getTcontent() {
+		return tcontent;
+	}
+	public void setTcontent(String tcontent) {
+		this.tcontent = tcontent;
+	}
+	
+}
