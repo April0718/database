8dc9b93bc7bb4f6c111db1abed3624bc141c925c
unknown <wuyuan1104>
2019-12-12 22:00:41 +0800

    first commit

--- CommentDAO.java
+++
@@ -1,8 +0,0 @@
-package DouBan;
-
-public interface CommentDAO {
-	public void insertComment(Comment com);
-	public void updateComment(Comment com);
-	public void deleteComment(int Uid,int Fid);
-	public Comment getComment(int Uid,int Fid);
-}
